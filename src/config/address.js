1e3202ff039d1fb5ae1104fdef5e376fce17a960
Kobina_Koomson <georgeranch31@gmail.com>
2025-07-03 04:13:16 +0000

    feat: add resume and address config files, and update navigation links
    Introduced `resume-data.js` and `address.js` for streamlined data management. Updated navigation configuration to include new resume and portfolio links while utilizing dynamic social links from the address config.

---
+++ src/config/address.js
@@ -0,0 +1,16 @@
+export const ADDRESS_DATA = {
+  email: "georgeranch31@gmail.com",
+  phone: "+233 55 069 6312",
+  secondaryPhone: "+233 26 072 1202",
+  correspondence: "P. O. Box NB 122, Nii Boiman, Accra",
+  location: "Cash link, Accra, Ghana",
+  social: {
+    facebook: "https://www.facebook.com/kobina.georgekoomson",
+    twitter: "https://twitter.com/georgeranch",
+    instagram: "https://www.instagram.com/kobina_georgekoomson/",
+    linkedin: "https://www.linkedin.com/in/kobina-koomson/",
+    github: "https://github.com/archX3",
+    behance: "https://www.behance.net/georgeranc8053",
+  },
+  website: "http://www.kobina.me",
+}
--- src/config/nav-config.js
+++ src/config/nav-config.js
@@ -1,43 +1,46 @@
+import { ADDRESS_DATA } from "@/config/address";
+
 export const HOME_NAV_TABLE = [
-  { name: 'Home', link: '#app', icon: 'home.svg' },
-  { name: 'About', link: '#about-section', icon: 'help.svg' },
-  { name : 'Blog', link : '#blog-section', icon : 'blog.svg' },
-  // { name: 'Portfolio', link: '#gallery-section', icon: 'picture.svg' },
+  // { name: 'Home', link: '#app', icon: 'home.svg' },
+  // { name: 'About', link: '#about-section', icon: 'help.svg' },
+  // { name : 'Blog', link : '#blog-section', icon : 'blog.svg' },
   // { name: 'Customers', link: '#customers', icon: 'happy-client.svg' },
-  // { name: 'Contact', link: '#contact', icon: 'headset.svg' }
+  { name: "Resume", link: "/resume", icon: "headset.svg" },
+  { name: "Portfolio", link: "/portfolio", icon: "picture.svg" },
+  { name: "Contact", link: "/contact", icon: "headset.svg" }
 ];
 
 export const EXTERNAL_NAV_LIST = [
-  { name: 'Blog', link: '#', icon: 'blog.svg' },
-  { name: 'Logofolio', link: 'logofolio', icon: 'logofolio.svg' },
+  { name: "Blog", link: "#", icon: "blog.svg" },
+  { name: "Logofolio", link: "logofolio", icon: "logofolio.svg" }
 ];
 
 export const EXTERNAL_LINKS = [
   {
-    name: 'External',
-    link: '#',
-    icon: 'github.svg',
+    name: "External",
+    link: "#",
+    icon: "github.svg",
     children: [
       {
-        name: 'Github',
-        link: 'https://github.com/archX3',
-        icon: 'github.svg',
+        name: "Github",
+        link: ADDRESS_DATA.social.github,
+        icon: "github.svg"
       },
       {
-        name: 'LinkedIn',
-        link: 'https://www.linkedin.com/in/kobina-koomson/',
-        icon: 'github.svg',
+        name: "LinkedIn",
+        link: ADDRESS_DATA.social.linkedin,
+        icon: "github.svg"
       },
       {
-        name: 'Behance',
-        link: 'https://www.behance.net/georgeranc8053',
-        icon: 'github.svg',
-      },
+        name: "Behance",
+        link: ADDRESS_DATA.social.behance,
+        icon: "github.svg"
+      }
       // {
       //   name : 'Freepik',
       //   link : 'https://github.com/archX3',
       //   icon : 'github.svg',
       // },
     ]
-  },
+  }
 ];
