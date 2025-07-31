53247af94bb8bfac1a0b8421a9dbfcd5f340bb5e
Kobina_Koomson <georgeranch31@gmail.com>
2025-07-31 07:08:44 +0000

    feat: add SEO metadata generation and HTML file creation for blog posts

--- scripts/generateBlogRoutes.js
+++ scripts/generateBlogRoutes.js
@@ -3,11 +3,13 @@ const path = require("path");
 const matter = require("gray-matter");
 
 const ELLIPSIS = "...";
-const DOMAIN = "http://kobin.me";
+const DOMAIN = "https://kobina.me";
+const BASE_URL = DOMAIN;
 const BLOG_DIR = path.join(__dirname, "../src/blogs");
 const ROUTE_FILE = path.join(__dirname, "../src/routes/blog-routes.js");
 const SITEMAP_FILE = path.join(__dirname, "../public/sitemap.xml");
 const POST_LIST_FILE = path.join(__dirname, "../src/blogs/posts.json");
+const PUBLIC_DIR = path.join(__dirname, "../public");
 
 function truncateString (str, maxLength, wordBoundary = false, ellipsis = true) {
   const suffix = ellipsis ? ELLIPSIS : "";
@@ -38,6 +40,41 @@ function createSitemapEntry ({ loc, changeFreq, priority, lastMod }) {
   </url>`;
 }
 
+/**
+ * creates the html for the seo entry for the blog post
+ * @param data {title: string, slug: string, description: string, cover: string, coverAlt: string, published: boolean}
+ * @returns {{data: {slug: string, title: string, description: string, cover: string, coverAlt: string, published: boolean}, content: string}}
+ */
+function createPostHtmlForSeoEntry (data) {
+  const { title, slug, description, cover, coverAlt, published } = data;
+  return {
+    data,
+    content: `
+  <title>${title}</title>
+  <meta name="title" content="${title}"/>
+  <meta name="description" content="${description}"/>
+  <meta name="image" content="${cover}"/>
+  <meta name="image:alt" content="${coverAlt}"/>
+  <meta name="robots" content="${published ? "index, follow" : "noindex, nofollow"}"/>
+  <meta property="og:title" content="${title}"/>
+  <meta property="og:description" content="${description}"/>
+  <meta property="og:image" content="${cover}"/>
+  <meta property="og:image:alt" content="${coverAlt}"/>
+  <meta property="og:type" content="article"/>
+  <meta property="og:url" content="${DOMAIN}/blog/${title}"/>
+  <meta name="twitter:card" content="summary_large_image"/>
+  <meta name="twitter:title" content="${title}"/>
+  <meta name="twitter:description" content="${description}"/>
+  <meta name="twitter:image" content="https://kobina.me${cover}"/>
+  <meta name="twitter:image:alt" content="${coverAlt}"/>
+  <meta name="twitter:creator" content="@georgeranch"/>
+  <meta name="twitter:site" content="@georgeranch"/>
+  <meta name="twitter:domain" content="${DOMAIN}"/>
+  <meta name="twitter:url" content="${DOMAIN}/blog/${slug}"/>
+`
+  };
+}
+
 function writeRoutesToFile (routes) {
   const routeCode = `
 export default [
@@ -64,6 +101,51 @@ ${sitemapEntries.join("\n")}
   fs.writeFileSync(SITEMAP_FILE, sitemap);
 
   console.log("✅ Blog routes and sitemap generated.");
+}
+
+/**
+ * write the html for the seo entry for the blog post to the file
+ * @param postsHtmlForSeo {Array<{data: {title: string, slug: string, description: string, cover: string, coverAlt: string, published: boolean}, content: string}>}
+ */
+function writePostHtmlForSeoToFile (postsHtmlForSeo) {
+  postsHtmlForSeo.forEach(post => {
+    const { data, content } = post;
+
+    const filePath = path.join(PUBLIC_DIR, `blog/${data.slug}.html`);
+    const blogDir = path.join(PUBLIC_DIR, "blog");
+    if (!fs.existsSync(blogDir)) {
+      fs.mkdirSync(blogDir);
+    }
+
+    const html = `
+    <!DOCTYPE html>
+<html lang="en" class="dark" data-theme="dark">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+    <link rel="icon" href="${BASE_URL}/favicon/favicon.jpg">
+    <link rel="icon" sizes="192x192" href="${BASE_URL}/favicon/favicon.jpg">
+    <link rel="icon" type="image/jpg" href="${BASE_URL}/favicon/favicon.jpg">
+    <!--[if IE]>
+    <link rel="shortcut icon" href="${BASE_URL}/favicon/favicon.jpg"/><![endif]-->
+   
+    <link rel="canonical" href="${BASE_URL}" />
+  ${content}
+  </head>
+  <body>
+    <noscript>
+      <strong>Sorry, this site doesn't work properly without JavaScript enabled. Please enable it and enjoy.</strong>
+    </noscript>
+    <div id="app"></div>
+  </body>
+</html>
+
+`;
+
+    fs.writeFileSync(filePath, html);
+  });
 }
 
 function writePostListToFile (postList) {
@@ -99,6 +181,7 @@ function main () {
       lastMod: new Date().toISOString()
     })
   ];
+  const postsHtmlForSeo = [];
 
   const files = fs.readdirSync(BLOG_DIR).filter(f => f.endsWith(".md"));
   const routes = [];
@@ -132,6 +215,16 @@ function main () {
         published: data.published || true
       });
 
+      postsHtmlForSeo.push(createPostHtmlForSeoEntry({
+        title: data.title || slug,
+        description: data.description || "",
+        cover: data.cover || "",
+        coverAlt: data.coverAlt || "",
+        thumbnail: data.thumbnail || "",
+        published: data.published || true,
+        slug
+      }));
+
       sitemapEntries.push(createSitemapEntry({
         loc: `${DOMAIN}${routePath}`,
         changeFreq: "weekly",
@@ -140,6 +233,8 @@ function main () {
       }));
     });
     writeRoutesToFile(routes);
+
+    writePostHtmlForSeoToFile(postsHtmlForSeo);
 
     writeSitemapToFile(sitemapEntries);
 
